4024823f824f90b5545d5436148de0a9320ba735
平野健太朗 <hiranokentaro@hiranokentarounoMacBook-Air.local>
2021-03-09 18:27:09 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'user strict'
+
+{
+    const word = 'red';
+    let loc = 0;
+
+    const target = document.getElementById('target');
+    target.textContent = word;
+
+    document.addEventListener('keydown', e => {
+        if (e.key === word[loc]) {
+            loc++;
+
+            //1: _ed
+            //2: __d
+            //3: ___
+            target.textContent = '_'.repeat(loc);
+        }
+    });
+
+}
