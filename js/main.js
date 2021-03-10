900b7291f5eab117b504f3ff7f9890971b96e819
平野健太朗 <yakyukentaro@gmail.com>
2021-03-10 18:48:00 +0900

    remake2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 'use strict'
 
 {
-    const word = 'red';
+
+    function setWord() {
+        word = words.splice([Math.floor(Math.random() * words.length)], 1)[0];
+        target.textContent = word;
+        loc = 0;
+    }
+
+    let words = ['red', 'blue', 'pink'];
+    let word;
     let loc = 0;
-
+    let startTime;
+    let isPlaying = false;
     const target = document.getElementById('target');
-    target.textContent = word;
 
+
+    document.addEventListener('click', () => {
+        if (isPlaying) {
+            return;
+        }
+        isPlaying = true;
+        startTime = Date.now();
+        setWord();
+    })
 
     document.addEventListener('keydown', e => {
         if (e.key !== word[loc]) {
@@ -14,5 +31,31 @@
         }
         loc++;
         target.textContent = '_'.repeat(loc) + word.substring(loc);
+
+        if (loc === word.length) {
+            if (words.length === 0) {
+                const elapesedTime = ((Date.now() - startTime) / 1000).toFixed(2);
+                const result = document.getElementById('result');
+                result.textContent = `Finished! ${elapesedTime} seconds`;
+                const restart = document.getElementById('restart');
+                restart.classList.add('restart');
+                restart.textContent = "Restart";
+                return;
+            }
+            setWord();
+        }
     });
+
+    reStart();
+
+    function reStart() {
+        restart.addEventListener('click', () => {
+            result.textContent = "";
+            restart.classList.remove('restart');
+            restart.textContent = "";
+            startTime = Date.now();
+            words = ['red', 'blue', 'pink'];
+            setWord();
+        });
+    }
 }
